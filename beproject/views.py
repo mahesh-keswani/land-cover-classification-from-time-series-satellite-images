3b18be2a9e0906aada6e1b2366ebadab6d84f506
mahesh-keswani <karthikkeswani1234@gmail.com>
2020-12-11 09:23:09 +0530

    added functions to get the satellite data in real time

--- beproject/views.py
+++ beproject/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import ee
+import numpy as np 
+from . import handleEEstuff 
 
 def homepage(request):
 	return render(request, 'map.html')
@@ -10,7 +13,30 @@ def analysis(request):
 		toDate = request.POST['toDate']
 		allLatLng = request.POST['allLatLng']
 
-		print("From date",fromDate)
-		print("toDate",toDate)
-		print("allLatLng",allLatLng)
+		allLatLng = allLatLng.split(",")
+
+		N = len(allLatLng)
+		latLongPairs = []
+		for i in range(0, N, 2):
+			if i == N - 1:
+				break
+			latLongPairs.append([float(allLatLng[i]), float(allLatLng[i + 1])])
+
+		(image1, n1), (image2, n2) = handleEEstuff.getData(fromDate, toDate, latLongPairs)
+
+		print(image1.shape, image2.shape)
+
 		return HttpResponse("analysis")
+
+
+
+
+
+
+
+
+
+
+
+
+
