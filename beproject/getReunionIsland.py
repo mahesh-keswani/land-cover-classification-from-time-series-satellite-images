007eef1b17a7a92a15135bf80292c10d84258153
mahesh-keswani <karthikkeswani1234@gmail.com>
2020-12-11 21:55:45 +0530

    loading and processing reunion data

---
+++ beproject/getReunionIsland.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+import pickle
+import numpy as np 
+
+def getReunionData():
+	arr = []
+	for line in open('C:/Users/renu/Desktop/django_projects/beproject/beproject/training.txt', 'r'):
+		line = list(map(float, line.split(",")))
+		arr.append(line)
+
+
+	arr = np.array(arr).reshape(23, 81714, 10)
+	sc = pickle.load(open('C:/Users/renu/Desktop/django_projects/beproject/beproject/my_scaler.pkl','rb'))
+
+	data = []
+	for single_arr in arr[:3]:
+		data.append(sc.transform(single_arr))
+	
+	return np.array(data)
