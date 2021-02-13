dc13dc580e5dff3bb8148f9fb15a86076486af49
Mahesh Keswani <karthikkeswani1234@gmail.com>
2021-02-13 18:32:55 +0530

    added the code for saving the predictions of reunion island
     For faster plotting and also plotting mean of every class on every day for checking the variation

--- beproject/views.py
+++ beproject/views.py
@@ -8,6 +8,9 @@ from . import modelStuff
 from plotly.offline import plot
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
+import reverse_geocoder as rg 
+import os
+import pandas as pd
 
 def homepage(request):
 	return render(request, 'map.html')
@@ -27,27 +30,29 @@ def analysis(request):
 				break
 			latLongPairs.append([float(allLatLng[i]), float(allLatLng[i + 1])])
 
-		area =  [ [ -86.88194274902344, 21.030673628606102],
-                 [-86.73431396484375, 21.030673628606102],
-                 [-86.73431396484375, 21.197216077387107],
-                 [-86.88194274902344, 21.197216077387107]
-          		]
-		(image1, n1), (image2, n2) = handleEEstuff.getData(fromDate, toDate, area)
+		# area =  [ [ -86.88194274902344, 21.030673628606102],
+  #                [-86.73431396484375, 21.030673628606102],
+  #                [-86.73431396484375, 21.197216077387107],
+  #                [-86.88194274902344, 21.197216077387107]
+  #         		]
+		images, n = handleEEstuff.getData(fromDate, toDate, latLongPairs)
 
-		if n1 == 1 and n2 == 1:
-			print("Shape of images", image1.shape, image2.shape)
-			plot_div = plot_graphs(image1, image2)
-			
-			return render(request, 'reunion.html', context={'plot_div': plot_div})
+		if n == 1:
+			# print(len())
+			# plot_div = plot_graphs(image1, image2)
+				
+			# reverse_latlon = rg.search(latLongPairs)[0]['name']
+			# print(reverse_latlon)
 
-		return HttpResponse("Server Error: " + str(image1) + "\n" + str(image2) )
+			# return render(request, 'reunion.html', context={'plot_div': plot_div})
+			return render(HttpResponse('Done!!!'))
+
+		return HttpResponse("Server Error: " + str(images) + "\n" + str(images) )
 
 def plot_graphs(image1, image2):
-	scaled_image1 = modelStuff.scale_data(image1)
-	scaled_image2 = modelStuff.scale_data(image2)
 
-	pred1 = modelStuff.getPrediction(scaled_image1)
-	pred2 = modelStuff.getPrediction(scaled_image2)
+	pred1 = modelStuff.getPrediction(image1)
+	pred2 = modelStuff.getPrediction(image2)
 
 	predictions = [pred1, pred2]
 	plot_div = plot_bars(predictions, "reunion.html", "Graphs for the real time data !!!")
@@ -57,13 +62,45 @@ def plot_graphs(image1, image2):
 def reunion(request):
 	X = getReunionIsland.getReunionData()
 
-	predictions = []
-	for x in X:
-		predictions.append(modelStuff.getPrediction(x))
+	if os.path.isfile('reunion_pred.csv'):
+		predictions = pd.read_csv('reunion_pred.csv').values
+	else:
+		predictions = []
+		for x in X:
+			predictions.append(modelStuff.getPrediction(x))
 
-	plot_div = plot_bars(predictions, "reunion.html", title = 'Reunion Island Data from Landsat-8 of the year 2014')
+		predictions = np.array(predictions).T
+		pd.DataFrame(data = predictions, columns = range(1, 24)).to_csv('reunion_pred.csv', index = False)
 
-	return render(request, 'reunion.html', context={'plot_div': plot_div})
+	
+	xs = list(range(1, 24))
+	classes_to_per = []
+	for day in range(23):
+		_, ys = np.unique(predictions[:, day], return_counts=True)
+		ys = ( ys / np.sum(ys) ) * 100
+		classes_to_per.append(ys.tolist())
+
+	# TODO: plot every row of classes_to_per on line chart
+	classes_to_per = np.array(classes_to_per).T.tolist()
+	# print(classes_to_per)
+	lines = []
+	classes = ["urban areas", "other built-up surfaces", "forests", 
+		           "sparse vegetation", "rocks and bare soil", "grassland", 
+		           "sugarcane crops", "other crops", "water"]
+
+	colors = ['green', 'red', 'blue', 'orange', 'cyan', 'yellow', 'black', 'magenta', 'brown', 'lightgreen'] 
+	fig = go.Figure()
+	for i in range(len(classes_to_per)):
+		scatter = go.Scatter(x=xs, y=classes_to_per[i],
+                     mode='lines', name=classes[i],
+                     opacity=0.8, marker_color=colors[i])
+		fig.add_trace(scatter)
+
+	plt_line_div = plot(fig, output_type='div')
+
+	plot_div = plot_bars(predictions.T.tolist(), "reunion.html", title = 'Reunion Island Data from Landsat-8 of the year 2014')
+
+	return render(request, 'reunion.html', context={'plot_div': plot_div, 'plt_line_div': plt_line_div})
 
 
 def plot_bars(predictions, template, title):
