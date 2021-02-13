6705769dd6929715293e58a7570f861dd5c737d2
Mahesh Keswani <karthikkeswani1234@gmail.com>
2021-02-13 18:29:42 +0530

    instead of taking first 3 images, taking all the images

--- beproject/getReunionIsland.py
+++ beproject/getReunionIsland.py
@@ -8,12 +8,12 @@ def getReunionData():
 		line = list(map(float, line.split(",")))
 		arr.append(line)
 
-
+# (81714, 230) -> (23, 81714, 10)
 	arr = np.array(arr).reshape(23, 81714, 10)
 	sc = pickle.load(open('C:/Users/renu/Desktop/django_projects/beproject/beproject/my_scaler.pkl','rb'))
 
 	data = []
-	for single_arr in arr[:3]:
+	for single_arr in arr:
 		data.append(sc.transform(single_arr))
 	
 	return np.array(data)
