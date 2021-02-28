6565c7ef844fb0d9d69390e814a55235ae2861a3
RahulKanwal <rahulkanwal1999@gmail.com>
2021-02-28 13:07:34 +0530

    Fixed map markers and some UI adjustments.

--- beproject/handleEEstuff.py
+++ beproject/handleEEstuff.py
@@ -1,7 +1,7 @@
 import ee
-import numpy as np 
-import pandas as pd 
-from cv2 import convertScaleAbs
+import numpy as np
+import pandas as pd
+# from cv2 import convertScaleAbs
 from . import save_img_to_drive
 
 
@@ -10,7 +10,7 @@ from . import save_img_to_drive
 
 # 	data = np.random.randn( np.random.randint(low = 10000, high = 30000), 10 )
 # 	return data, 1
-	
+
 
 def getData(fromDate, toDate, geomtery):
 	save_img_to_drive.getData(fromDate, toDate, geomtery)
@@ -18,17 +18,3 @@ def getData(fromDate, toDate, geomtery):
 	image1, image2 = 1, 1
 	n1, n2 = 0, 0
 	return (image1, n1), (image2, n2)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
