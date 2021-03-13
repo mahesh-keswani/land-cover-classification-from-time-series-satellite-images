95149936f3b05ff1dba87d56edd3ee33cec51080
Mahesh Keswani <karthikkeswani1234@gmail.com>
2021-03-13 12:05:19 +0530

    optimized loading data for reunion and added function for preloaded tif images

--- beproject/getReunionIsland.py
+++ beproject/getReunionIsland.py
@@ -2,18 +2,35 @@ import pandas as pd
 import pickle
 import numpy as np 
 
+from osgeo import gdal
+
 def getReunionData():
-	arr = []
-	for line in open('C:/Users/renu/Desktop/django_projects/beproject/beproject/training.txt', 'r'):
-		line = list(map(float, line.split(",")))
-		arr.append(line)
+	df = pd.read_csv('training.txt', header =None)
 
-# (81714, 230) -> (23, 81714, 10)
-	arr = np.array(arr).reshape(23, 81714, 10)
-	sc = pickle.load(open('C:/Users/renu/Desktop/django_projects/beproject/beproject/my_scaler.pkl','rb'))
-
-	data = []
-	for single_arr in arr:
-		data.append(sc.transform(single_arr))
+	arr = df.values.reshape(23 , 81714, 10)
 	
-	return np.array(data)
+	return arr
+
+def getPreloadedX(name):
+	raster = gdal.Open(name)
+	imarray = np.array(raster.ReadAsArray())
+	imarray = imarray.reshape((imarray.shape[0], -1)).T
+
+	# (NIR - R) / (NIR + R)
+	ndvi = (imarray[:, 4] - imarray[:, 3]) / ( imarray[:, 4] + imarray[:, 3] )
+
+
+	# (G - NIR) / (G + NIR)			
+	ndwi = (imarray[:, 2] - imarray[:, 4]) / ( imarray[:, 2] + imarray[:, 4] )			
+
+	# Brightness Index = sqrt(((Red * Red)/ (Green* Green))/2)
+	bi = np.sqrt( ( (imarray[:, 3]*imarray[:, 3]) / (imarray[:, 2]*imarray[:, 2]) ) / 2 )
+
+	Arr_norm = np.concatenate(( imarray, ndvi.reshape(-1, 1), ndwi.reshape(-1, 1), bi.reshape(-1, 1) ), axis = 1)
+
+	df = pd.DataFrame(Arr_norm)
+	df.replace([np.inf, -np.inf], np.nan, inplace = True)
+	df.fillna(df.median(), inplace = True)	
+			
+	return df.values
+
